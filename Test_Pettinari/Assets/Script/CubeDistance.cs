951061991c60ca019a5ebefb1f2bc45b62f02166
esterPett <pettinariester@gmail.com>
2024-11-08 12:13:52 +0100

    Interazione_Cubi_Pareti_Pt2

--- Test_Pettinari/Assets/Script/CubeDistance.cs
+++ Test_Pettinari/Assets/Script/CubeDistance.cs
@@ -6,26 +6,35 @@ using UnityEngine.UIElements;
 
 public class CubeDistance : MonoBehaviour
 {
-    public  Transform originPosition;
+    
     [SerializeField] private GameObject cube;
-    public float distance;
+    [SerializeField] private float meshConstant; //Costante per regolare la distanza tra il cubo e la superficie 
+    public Transform originPosition;
+    public float distance; //Distanza massima del Raycast
     public LayerMask parete;
+    
     void Update()
     {
-        cube.transform.LookAt(originPosition);
-        Vector3 direction = (transform.position - originPosition.position).normalized;
+        cube.transform.LookAt(originPosition); //Ruoto il cubo in modo che guardi sempre verso l'originPosition
+        //Calcola la direzione normalizzata dal cubo verso l'origine
+        Vector3 direction = (transform.position- originPosition.position).normalized;
         RaycastHit hit = new RaycastHit();
 
-        if (Physics.Raycast(originPosition.position, transform.position, out hit, distance , parete))
-        {
-            cube.transform.position = hit.point - direction;
-           
-        }
-        else
-        {
-            cube.transform.position = transform.position;
-        }
+         if (Physics.Raycast(transform.position,-direction, out hit, distance , parete))
 
-        Debug.DrawLine(originPosition.position, transform.position);
+          {
+            cube.transform.position = hit.point - (hit.normal * meshConstant);
+          
+          }
+          else
+
+          {
+              cube.transform.position = transform.position;
+          }
+
+          Debug.DrawLine(originPosition.position, transform.position);
+
+        
     }
 }
+
