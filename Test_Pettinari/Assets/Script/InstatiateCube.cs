e81dee743d9fb831560948dd27752a8c84d518d4
esterPett <pettinariester@gmail.com>
2024-11-08 16:27:27 +0100

    Assegnazione_Colori_Cubi

--- Test_Pettinari/Assets/Script/InstatiateCube.cs
+++ Test_Pettinari/Assets/Script/InstatiateCube.cs
@@ -9,8 +9,9 @@ public class InstatiateCube : MonoBehaviour
     [SerializeField] private float distance= 5f; //Distanza dei cubi dal cilindro
     [SerializeField] private GameObject cube; //Collegamento al GameObject cube
     [SerializeField] private LayerMask parete;
-
     [SerializeField] private List<GameObject> cubi;
+    [SerializeField] private Material pari;
+    [SerializeField] private Material dispari;
     void Start()
     {
         float distanceCube = 360f / nCube; //Distanza tra ogni cubo
@@ -24,9 +25,19 @@ public class InstatiateCube : MonoBehaviour
 
             Vector3 position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
 
-            
-          
             GameObject obj = Instantiate(cube, position, Quaternion.identity, transform);
+
+            
+           
+            //Assegno il materiale in baee se i cubi sono pari o dispari
+            if (i % 2 == 0)
+            {
+                obj.transform.GetChild(0).GetComponent<Renderer>().material = pari;
+            }
+            else
+            {
+                obj.transform.GetChild(0).GetComponent<Renderer>().material = dispari;
+            }
             cubi.Add(obj);
 
             obj.GetComponent<CubeDistance>().originPosition = transform;
@@ -35,6 +46,7 @@ public class InstatiateCube : MonoBehaviour
 
     }
 
+  
     private void OnDrawGizmos()
     {
         Debug.DrawLine(transform.position, transform.position-transform.right *distance);
