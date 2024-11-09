3b8afdc65d11821aa9c94f8a8558f367afc4e0b3
esterPett <pettinariester@gmail.com>
2024-11-09 19:22:32 +0100

    Interazione_Cubi_Parete_Pt2
    Ho cercato di migliorare l'interazione fra i cubi e le pareti, ma la devo perfezionare ancora

--- Test_Pettinari/Assets/Script/CubeDistance.cs
+++ Test_Pettinari/Assets/Script/CubeDistance.cs
@@ -16,21 +16,22 @@ public class CubeDistance : MonoBehaviour
     void Update()
     {
         cube.transform.LookAt(originPosition); //Ruoto il cubo in modo che guardi sempre verso l'originPosition
+       
         //Calcola la direzione normalizzata dal cubo verso l'origine
-        Vector3 direction = (transform.position- originPosition.position).normalized;
+        Vector3 direction = (transform.position - originPosition.position).normalized;
         RaycastHit hit = new RaycastHit();
-
-         if (Physics.Raycast(transform.position,-direction, out hit, distance, parete))
+     
+         if (Physics.Raycast(transform.position + direction * 4 ,-direction, out hit, distance, parete))
 
           {
-            cube.transform.position = hit.point - (hit.normal * meshConstant);
-
-        }
+            Vector3 nuovaPosizione = hit.point - (hit.normal * meshConstant);
+            cube.transform.position = Vector3.Lerp(cube.transform.position, nuovaPosizione, 0.5f);
+          }
           else
 
           {
-              cube.transform.position = transform.position;
-          }
+            cube.transform.position = Vector3.Lerp(cube.transform.position, transform.position, 0.5f);
+        }
 
           Debug.DrawLine(originPosition.position, transform.position);
 
